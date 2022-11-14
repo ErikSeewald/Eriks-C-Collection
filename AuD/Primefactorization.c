f58024372bd027f97e3a36d6f1b4514277cb8508
Erik Seewald <erik.seewaldx@gmail.com>
2022-11-14 17:01:56 +0100

    clarification for Primefactorization.c

--- AuD/Primefactorization.c
+++ AuD/Primefactorization.c
@@ -1,31 +1,54 @@
 # include <stdio.h>
 # include <math.h>
 
-void primeFactors(int n)
+int primfac(int a[], int s)
 {
-    while (n%2 == 0)
+    int arrIndex = 0;
+
+    while (s%2 == 0)
     {
-        printf("%d ", 2);
-        n = n/2;
+        a[arrIndex] = 2;
+        ++arrIndex;
+        s = s/2;
     }
 
     // n is odd at this point
-    for (int i = 3; i <= sqrt(n); i = i+2)
+    for (int i = 3; i <= sqrt(s); i = i+2)
     {
-        while (n%i == 0)
+        while (s%i == 0)
         {
-            printf("%d ", i);
-            n = n/i;
+            a[arrIndex] = i;
+            ++arrIndex;
+            s = s/i;
         }
     }
 
-    if (n > 2)
-    {printf ("%d ", n);}
+    //means we just exited out of the for loop instead of going immediately from the first loop to the end
+    //only needed if you only go to sqrt(s)
+    if (s > 2)
+    {
+        a[arrIndex] = s;
+        ++arrIndex;
+    }
+
+    return arrIndex;
+}
+
+void printArray(int a[], int z)
+{
+    for (int i = 0; i < z; i++)
+    {
+        printf("%d\n", a[i]);
+    }
 }
 
 int main()
 {
-    int n = 455;
-    primeFactors(n);
+    int a[24];
+    int s = sizeof(a) / sizeof(a[0]);
+    int z = primfac(a, s);
+    printf("Amount of prime factors: %d\n",z);
+    printf("Prime factorization:\n");
+    printArray(a, z);
     return 0;
 }
